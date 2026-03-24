abd2aa1069de8bcad8aded3f2807ac092f5fd480
Simon Mika <simon@mika.se>
2026-03-24 15:07:12 +0100

    Old configuration.

---
+++ Context/Article/Section.ts
@@ -0,0 +1,20 @@
+import { Site } from "../../Site"
+
+export interface Section {
+	id: string
+	link?: string
+	type?: string
+	title?: string
+	content?: string
+}
+export namespace Section {
+	export function load(section: Site.Page.Section & { path: Site.Page.Path }): Section {
+		return {
+			id: section.path.fragment ?? "",
+			link: section.path.toString(),
+			type: section.type,
+			title: section.title,
+			content: section.content,
+		}
+	}
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+import { Context as _Context } from "./Context"
+import { Site as _Site } from "./Site"
+
+export namespace binotype {
+	export import Context = _Context
+	export import Site = _Site
+}
